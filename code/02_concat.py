0bf3129d2f49c4b1d1da0521b3512cd6b713a3ff
jinhoheoo <heojh2580@gmail.com>
2024-02-06 12:04:08 +0900

    concat, preprocessing

---
+++ code/02_concat.py
@@ -0,0 +1,17 @@
+data_paths = glob.glob('./data_naver/*')
+print(data_paths)
+
+df = pd.DataFrame()
+
+for path in data_paths:
+    df_temp = pd.read_csv(path)
+    df_temp.columns = ['names', 'reviews']
+    df_temp.dropna(inplace=True)
+    df = pd.concat([df, df_temp], ignore_index=True)
+
+df.drop_duplicates(inplace=True)
+df = df.loc[df.groupby('names')['reviews'].apply(lambda x: x.str.len().idxmax())].reset_index(drop=True)
+df = df[df['reviews'].apply(lambda x: len(str(x)) > 5000)]
+df.info()
+df.to_csv('./reviews_naver.csv', index=False)
+
