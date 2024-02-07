9abe28fcae26461dfc643bf7d498223a0cae3c23
pc1 <cute_js@daum.net>
2024-02-07 16:44:13 +0900

    불완정

---
+++ job06_01_movie_recommendation.py
@@ -0,0 +1,99 @@
+import pandas as pd
+from sklearn.metrics.pairwise import  linear_kernel
+from scipy.io import mmread
+import pickle
+from konlpy.tag import Okt
+from gensim.models import Word2Vec
+import numpy as np
+
+
+def getRecommendation(consine_sim):
+
+    simScore = list(enumerate(consine_sim[-1])) #인덱스 유지위해
+    simScore = sorted(simScore, key=lambda x:x[1], reverse=True)
+    simScore = simScore
+    resIdx = [i[0] for i in simScore]
+    resList = df_reviews.iloc[resIdx,0]
+    return resList
+
+
+df_reviews = pd.read_csv('./res_naver_sum.csv')
+Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
+with open('./models/tfidf.picle','rb') as f:
+    Tfidf = pickle.load(f)
+
+embedding_model = Word2Vec.load('./models/word2vec_res_review.model')
+keyword = ['김치찌개']
+list_mul = [10,5,1]
+recommendations = []
+simLists=[]
+
+sentence = []
+for idx,key_list in enumerate(keyword):
+    sim_word = embedding_model.wv.most_similar(key_list, topn=3)
+    words = [key_list]
+    for word, _ in sim_word:
+        words.append(word)
+
+    count = list_mul[idx]
+    for word in words:
+        sentence = sentence + [word] * count
+        count -=1
+
+print(sentence)
+sentence = ' '.join(sentence)
+
+sentence_vec = Tfidf.transform([sentence])
+cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
+# simLists.append(cosine_sim)
+#
+# sim_sum = np.array([[0.0] * 1445])
+# for idx, simscore in enumerate(simLists):
+#     sim_sum += sim_sum + simscore * list_mul[idx]
+
+recommendation = getRecommendation(cosine_sim)
+
+
+
+
+results = df_reviews.iloc[recommendation.index]
+count=0
+#print(len(recommendation.index))
+for i in range(0,len(recommendation.index)):
+    if len(results.iloc[i,1])>5000 :
+        if results.iloc[i,2]=='고양시':
+            if count < 30:
+                print(results.iloc[i,0])
+                count +=1
+            else:
+                break
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- job06_food_recommendation.py
+++ job06_food_recommendation.py
@@ -7,16 +7,15 @@ from gensim.models import Word2Vec
 import numpy as np
 
 
-def getRecommendation(consine_sim,num):
+def getRecommendation(consine_sim):
 
     simScore = list(enumerate(consine_sim[-1])) #인덱스 유지위해
     simScore = sorted(simScore, key=lambda x:x[1], reverse=True)
-    simScore = simScore[:num]
+
     resIdx = [i[0] for i in simScore]
     resList = df_reviews.iloc[resIdx,0]
-    return resList[:10]
-    return recmovieList[0:num]
-ref_idx = 6
+    return resList
+
 
 df_reviews = pd.read_csv('./res_naver_sum.csv')
 Tfidf_matrix = mmread('./models/Tfidf_movie_review.mtx').tocsr()
@@ -24,48 +23,56 @@ with open('./models/tfidf.picle','rb') as f:
     Tfidf = pickle.load(f)
 
 embedding_model = Word2Vec.load('./models/word2vec_res_review.model')
-keyword = ['생선','고급']
-list_mul = [2,1.5,1.3,1.1,1]
+keyword = ['콩나물','국밥']
+list_mul = [10,7,1]
 recommendations = []
 simLists=[]
-for key_list in keyword:
+
+
+for idx,key_list in enumerate(keyword):
     sim_word = embedding_model.wv.most_similar(key_list, topn=10)
     words = [key_list]
     for word, _ in sim_word:
         words.append(word)
-    sentence = []
-    count = 10
+
+    sentence=[]
+    count = list_mul[idx]
     for word in words:
         sentence = sentence + [word] * count
         count -=1
+
     sentence = ' '.join(sentence)
-    print(sentence)
     sentence_vec = Tfidf.transform([sentence])
     cosine_sim = linear_kernel(sentence_vec, Tfidf_matrix)
     simLists.append(cosine_sim)
-
+    print(sentence)
 sim_sum = np.array([[0.0] * 1445])
 for idx, simscore in enumerate(simLists):
     sim_sum += sim_sum + simscore * list_mul[idx]
 
-recommendation = getRecommendation(sim_sum,50)
-print(recommendation)
+
+recommendation = getRecommendation(sim_sum)
+#print(recommendation)
+#순위별로
+print(recommendation.info())
+results=df_reviews.iloc[recommendation.index]
+print(results)
+count=0
+for i in range(0,len(results['names'])):
+    if len(results.iloc[i,1])>15000:
+        if results.iloc[i,2] == '고양시':
+            if count<1:
+                count+=1
+                print(results.iloc[i,0])
+            else:
+                break
 
 
 
 
 
 
-# food_result = pd.DataFrame({'names':'','simscore':0,'orderbyu':0})
-# for recommendation in recommendations:
-#     food_result = food_result + recommendation
-#
-# oriList = list(recommendations[0]) + list(recommendations[1])
-#
-#
-#
-#
-# print(dupList)
+
 
 
 
